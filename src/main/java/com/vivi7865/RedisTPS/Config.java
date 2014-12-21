37aa4f8a6a52e622aaf23793bee072bfc401cd80
vivi7865 <me@vivi7865.com>
2014-12-21 18:15:09 +0100

    Code clean up, added Metrics (MCstats)

--- src/main/java/com/vivi7865/RedisTPS/Config.java
+++ src/main/java/com/vivi7865/RedisTPS/Config.java
@@ -23,59 +23,72 @@ public class Config {
 		this.plugin = plug;
 		file = new File(dataFolder, "config.yml");
 		conf = YamlConfiguration.loadConfiguration(file);
-		
+
 		final String redisServer = conf.getString("redis-server", "localhost");
 		final int redisPort = conf.getInt("redis-port", 6379);
-        String redisPassword = conf.getString("redis-password");
-        String serverID = conf.getString("server-id");
-        ntpHost = conf.getString("NTP-host");
-        checkInterval = conf.getInt("check-interval", 3);
-		
-        if (redisPassword != null && (redisPassword.isEmpty() || redisPassword.equals("none"))) {
-            redisPassword = null;
-        }
-        
-        if (serverID == null || serverID.isEmpty()) {
-            throw new RuntimeException("server-id not specified in configuration or empty");
-        }
-        plugin.setServerID(serverID);
+		String redisPassword = conf.getString("redis-password");
+		String serverID = conf.getString("server-id");
+		ntpHost = conf.getString("NTP-host");
+		checkInterval = conf.getInt("check-interval", 3);
 
-        if (redisServer != null && !redisServer.isEmpty()) {
-            final String finalRedisPassword = redisPassword;
-            Bukkit.getScheduler().runTask(plugin, new Runnable() {
+		if (redisPassword != null
+				&& (redisPassword.isEmpty() || redisPassword.equals("none"))) {
+			redisPassword = null;
+		}
+
+		if (serverID == null || serverID.isEmpty()) {
+			throw new RuntimeException(
+					"server-id not specified in configuration or empty");
+		}
+		plugin.setServerID(serverID);
+
+		if (redisServer != null && !redisServer.isEmpty()) {
+			final String finalRedisPassword = redisPassword;
+			Bukkit.getScheduler().runTask(plugin, new Runnable() {
 
 				public void run() {
 					Jedis rsc = null;
 					try {
-	                    JedisPoolConfig config = new JedisPoolConfig();
-	                    config.setMaxTotal(-1);
-	                    config.setJmxEnabled(false);
-	                    plugin.setPool(new JedisPool(config, redisServer, redisPort, 0, finalRedisPassword));
-	                    rsc = plugin.getPool().getResource();
-	                    if (rsc.hexists("RedisTPS_heartbeats", plugin.getServerID())) {
-	                    	Long lastHB = Long.parseLong(rsc.hget("RedisTPS_heartbeats", plugin.getServerID()));
-	                    	if (lastHB != null && plugin.getTime() < lastHB + 15000) {
-	                    		plugin.getLogger().log(Level.SEVERE, "This instance is a possible imposter instance");
-	                    		plugin.getLogger().log(Level.SEVERE, "For security reason RedisTPS will now disable itself");
-	                    		plugin.getLogger().log(Level.SEVERE, "If this instance restart from crash please wait 15 seconds before restart");
-	                    		throw new RuntimeException("RedisTPS possible imposter instance");
-	                    	}
-	                    }
-					} catch (NumberFormatException ignored) {} catch (JedisConnectionException e) {
+						JedisPoolConfig config = new JedisPoolConfig();
+						config.setMaxTotal(-1);
+						config.setJmxEnabled(false);
+						plugin.setPool(new JedisPool(config, redisServer,
+								redisPort, 0, finalRedisPassword));
+						rsc = plugin.getPool().getResource();
+						if (rsc.hexists("RedisTPS_heartbeats",
+								plugin.getServerID())) {
+							Long lastHB = Long.parseLong(rsc.hget(
+									"RedisTPS_heartbeats", plugin.getServerID()));
+							if (lastHB != null
+									&& plugin.getTime() < lastHB + 15000) {
+								plugin.getLogger()
+										.log(Level.SEVERE,
+												"This instance is a possible imposter instance");
+								plugin.getLogger()
+										.log(Level.SEVERE,
+												"For security reason RedisTPS will now disable itself");
+								plugin.getLogger()
+										.log(Level.SEVERE,
+												"If this instance restart from crash please wait 15 seconds before restart");
+								throw new RuntimeException(
+										"RedisTPS possible imposter instance");
+							}
+						}
+					} catch (NumberFormatException ignored) {
+					} catch (JedisConnectionException e) {
 						if (rsc != null)
-		                    plugin.getPool().returnBrokenResource(rsc);
+							plugin.getPool().returnBrokenResource(rsc);
 						plugin.getPool().destroy();
 						plugin.setPool(null);
-		                rsc = null;
-		                throw e;
+						rsc = null;
+						throw e;
 					}
 				}
-            });
-            
-            
-        }
+			});
+
+		}
 	}
-	
+
 	public static String getNTPHost() {
 		return ntpHost;
 	}
--- src/main/java/com/vivi7865/RedisTPS/RedisTPS.java
+++ src/main/java/com/vivi7865/RedisTPS/RedisTPS.java
@@ -12,8 +12,8 @@ import org.apache.commons.net.ntp.NTPUDPClient;
 import org.apache.commons.net.ntp.TimeInfo;
 import org.bukkit.Bukkit;
 import org.bukkit.event.Listener;
-import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.mcstats.Metrics;
 
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
@@ -22,8 +22,6 @@ import redis.clients.jedis.Response;
 import redis.clients.jedis.exceptions.JedisConnectionException;
 
 public class RedisTPS extends JavaPlugin implements Listener {
-	@SuppressWarnings("unused")
-	private static PluginDescriptionFile plugDesc;
 	public static File dataFolder;
 	static JedisPool pool;
 	static String serverID;
@@ -32,85 +30,102 @@ public class RedisTPS extends JavaPlugin implements Listener {
 		Bukkit.getScheduler().cancelTasks(this);
 		Jedis rsc = pool.getResource();
 		try {
-            rsc.hdel("RedisTPS_heartbeats", serverID);
-            rsc.hdel("RedisTPS_TPS", serverID);
-            rsc.hdel("RedisTPS_Players", serverID);
-        } finally {
-            pool.returnResource(rsc);
-        }
-        pool.destroy();
+			rsc.hdel("RedisTPS_heartbeats", serverID);
+			rsc.hdel("RedisTPS_TPS", serverID);
+			rsc.hdel("RedisTPS_Players", serverID);
+		} finally {
+			pool.returnResource(rsc);
+		}
+		pool.destroy();
 	}
-
 
 	public void onEnable() {
+
+		try {
+			Metrics metrics = new Metrics(this);
+			metrics.start();
+		} catch (IOException ignore) {}
+
 		dataFolder = this.getDataFolder();
-		plugDesc = this.getDescription();
-		
+
 		saveDefaultConfig();
 		new Config(this, dataFolder);
-		
-		
+
 		Bukkit.getPluginManager().registerEvents(this, this);
-		
+
 		Bukkit.getServer().getScheduler().scheduleSyncRepeatingTask(this, new TPS(), 100L, 1L);
-		
+
 		Bukkit.getScheduler().runTaskTimerAsynchronously(this, new Runnable() {
-            public void run() {
-            	if(pool == null){
-            		getLogger().log(Level.WARNING, "No connection to Redis server");
-            		return;
-            	}
-            	long time = getTime();
-            	Jedis rsc = pool.getResource();
-            	
-                try {
-                    Pipeline pipeline = rsc.pipelined();
-                    pipeline.hset("RedisTPS_heartbeats", serverID, String.valueOf(time));
-                    pipeline.hset("RedisTPS_TPS", serverID, String.valueOf(Math.round(TPS.getTPS(100) * 100.0) / 100.0d));
-                    pipeline.hset("RedisTPS_Players", serverID, String.valueOf(Bukkit.getOnlinePlayers().length));
-                    pipeline.expire("RedisTPS_heartbeats", 4);
-                    Response<Map<String, String>> response = pipeline.hgetAll("RedisTPS_heartbeats");
-                    pipeline.sync();
-                    
-                    for (String key : response.get().keySet()) {
-                    	if (key == serverID) continue;
-                    	
-                    	if ((time - Long.parseLong(rsc.hget("RedisTPS_heartbeats", key))) > (20*Config.getCheckInterval()) + 500) {
-                    		getLogger().log(Level.WARNING, "Server " + key + " has no refresh hearbeat for 3 seconds, did it crash ?");
-                    		rsc.hdel("RedisTPS_heartbeats", key);
-                    	}
-                    }
-                    
-                } catch (JedisConnectionException e) {
-                    getLogger().log(Level.SEVERE, "Unable to refresh heartbeat, did your Redis server go away?", e);
-                    pool.returnBrokenResource(rsc);
-                } finally {
-                	pool.returnResource(rsc);
-                }
-            }
-        }, 20*Config.getCheckInterval(), 20*Config.getCheckInterval());
-		
+			public void run() {
+				if (pool == null) {
+					getLogger().log(Level.WARNING,
+							"No connection to Redis server");
+					return;
+				}
+				long time = getTime();
+				Jedis rsc = pool.getResource();
+
+				try {
+					Pipeline pipeline = rsc.pipelined();
+					pipeline.hset("RedisTPS_heartbeats", serverID,
+							String.valueOf(time));
+					pipeline.hset("RedisTPS_TPS", serverID, String.valueOf(Math
+							.round(TPS.getTPS(100) * 100.0) / 100.0d));
+					pipeline.hset("RedisTPS_Players", serverID,
+							String.valueOf(Bukkit.getOnlinePlayers().length));
+					pipeline.expire("RedisTPS_heartbeats", 4);
+					Response<Map<String, String>> response = pipeline
+							.hgetAll("RedisTPS_heartbeats");
+					pipeline.sync();
+
+					for (String key : response.get().keySet()) {
+						if (key == serverID)
+							continue;
+
+						if ((time - Long.parseLong(rsc.hget("RedisTPS_heartbeats", key))) > (20 * Config.getCheckInterval()) + 500) {
+							getLogger().log(Level.WARNING, "Server " + key + " has no refreshed hearbeat for 3 seconds, did it crash ?");
+							rsc.hdel("RedisTPS_heartbeats", key);
+						}
+					}
+
+				} catch (JedisConnectionException e) {
+					getLogger()
+							.log(Level.SEVERE,
+									"Unable to refresh heartbeat, did your Redis server go away?",
+									e);
+					pool.returnBrokenResource(rsc);
+				} finally {
+					pool.returnResource(rsc);
+				}
+			}
+		}, 20 * Config.getCheckInterval(), 20 * Config.getCheckInterval());
+
 	}
-	
+
 	public long getTime() {
 		if (Config.getNTPHost() != null || Config.getNTPHost() != "") {
-			try { 
+			try {
 				NTPUDPClient timeClient = new NTPUDPClient();
 				InetAddress inetAddress;
 				inetAddress = InetAddress.getByName(Config.getNTPHost());
 				TimeInfo timeInfo = timeClient.getTime(inetAddress);
-				long returnTime = timeInfo.getMessage().getTransmitTimeStamp().getTime();
+				long returnTime = timeInfo.getMessage().getTransmitTimeStamp()
+						.getTime();
 				Date time = new Date(returnTime);
 				return time.getTime();
 			} catch (UnknownHostException e) {
-				getLogger().log(Level.SEVERE, "Unknown host, did your NTP server host is wrong?", e);
+				getLogger().log(Level.SEVERE,
+						"Unknown host, did your NTP server host is wrong?", e);
 			} catch (IOException e) {
-				getLogger().log(Level.SEVERE, "Unable to get time from NTP server, did your NTP server go away?", e);
+				getLogger()
+						.log(Level.SEVERE,
+								"Unable to get time from NTP server, did your NTP server go away?",
+								e);
 			}
 		}
 		return System.currentTimeMillis();
 	}
-	
+
 	public void setServerID(String serverID) {
 		RedisTPS.serverID = serverID;
 	}
@@ -119,7 +134,6 @@ public class RedisTPS extends JavaPlugin implements Listener {
 		return serverID;
 	}
 
-
 	public JedisPool getPool() {
 		return pool;
 	}
@@ -127,5 +141,5 @@ public class RedisTPS extends JavaPlugin implements Listener {
 	public void setPool(JedisPool pool) {
 		RedisTPS.pool = pool;
 	}
-	
+
 }
